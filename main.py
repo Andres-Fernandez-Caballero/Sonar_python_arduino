3b87ba5b27ac93ed29c966ce2593ecec7ef5e3f4
lezzzca <lezzzca@gmail.com>
2020-11-02 23:38:32 -0300

    reparado la funcion main para la presentacion del prototipo

--- main.py
+++ main.py
@@ -2,20 +2,27 @@
 from mvc.Models.ConectorUsb import  ConectorUsb
 from mvc.Models.Sonar import Sonar
 
+
 def main():
     #controller = MainController()
     #controller.iniciar()
 
     # TODO: prueba de metodos de clase Sonar (eliminar despues)
     lista = ConectorUsb.getArduinoPorts()
-    puerto = lista[0]
-    print(ConectorUsb.getNombre(puerto))
 
-    sonar = Sonar(3, 4, 5)
+    if len(lista) != 0:
+        puerto = lista[0]
 
-    sonar.conectar(ConectorUsb.getNombre(puerto))
+        print(ConectorUsb.getNombre(puerto))
 
-    sonar.mover(90)
+        sonar = Sonar(3, 4, 5)
+
+        sonar.conectar(ConectorUsb.getNombre(puerto))
+
+        sonar.mover(90)
+    else:
+        print("no hay arduinos conectados")
+
 
 if __name__ == '__main__':
     main()
